ca71feb3da255b8a3962acb25de7df07cda3bfd9
mevlüt çifcibaşı <cifcibasi.mevlut@gmail.com>
2023-01-31 18:39:01 +0300

    Bismillah, 'In the Name of Allah (C.C)',is the start of all things good

---
+++ DateTimeExtensions.cs
@@ -0,0 +1,32 @@
+namespace Clean.Extensions.DateTime;
+
+public static class DateTimeExtensions
+{
+    public static System.DateTime FirstDayOfMonth()
+    {
+        return FirstDayOfMonth(new System.DateTime());
+    }
+
+    public static System.DateTime FirstDayOfMonth(this in System.DateTime value)
+    {
+        return new System.DateTime(value.Year, value.Month, 1);
+    } 
+    
+    public static System.DateTime FirstDayOfYear(this in System.DateTime value)
+    {
+        return new System.DateTime(value.Year, 1, 1);
+    }
+
+    public static System.DateTime LastDayOfMonth(this in System.DateTime value)
+    {
+        return value.FirstDayOfMonth()
+            .AddMonths(1)
+            .AddMilliseconds(-1);
+    }
+    
+    public static System.DateTime ToEndOfDay(this in System.DateTime time)
+    {
+        TimeSpan endOfDaySpan = TimeSpan.FromDays(1).Subtract(TimeSpan.FromMilliseconds(1));
+        return time.Date.Add(endOfDaySpan);
+    }
+}
